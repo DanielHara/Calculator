62e612b7b1b7c731183ca56a48775f8aea8675a8
Daniel Hara <haradaniel734@gmail.com>
2022-06-25 14:28:29 +0200

    setting up font

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=League+Spartan" />
     <App />
   </React.StrictMode>
 );
